5ca23af4dc28b49431f7a715d004c2da724bc021
shahrizoda <shahrizodadeveloping@gmail.com>
2022-02-27 20:21:36 +0500

    typing

---
+++ script.js
@@ -0,0 +1,20 @@
+const texts = ['websites', 'front of websites', 'pancakes'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = 0;
+
+(function type() {
+    if(count === texts.length){
+        count = 0
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 400)
+}())
